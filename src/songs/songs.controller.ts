1833cb9cd441d07ba23bb852986d3f65618b1a24
ahoang2502 <athoang252@gmail.com>
2024-03-28 22:24:46 +1100

    1 create a Service

---
+++ src/songs/songs.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+import { SongsService } from './songs.service';
+
+@Controller('songs')
+export class SongsController {
+  constructor(private songsService: SongsService) {}
+
+  @Post()
+  create() {
+    return this.songsService.create('Animals by Martin Garrix');
+  }
+
+  @Get()
+  findAll() {
+    return this.songsService.findAll();
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'fetch song based on Id';
+  }
+
+  @Put(':id')
+  update() {
+    return 'update song based on Id';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'delete song based on Id';
+  }
+}
---
+++ src/songs/songs.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SongsService {
+  // local db
+  // local array
+
+  private readonly songs = [];
+
+  create(song) {
+    // Save the song in the database
+    this.songs.push(song);
+
+    return this.songs;
+  }
+
+  findAll() {
+    // Fetch all songs from database
+    return this.songs;
+  }
+}
